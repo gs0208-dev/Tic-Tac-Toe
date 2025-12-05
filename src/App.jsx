7512839b55e6f87b1717180c2091b57ba146be2d
Giridhar S <giridhar200402@gmail.com>
2025-12-05 22:36:24 +0530

    completed the tic-tac-toe game.

--- src/App.jsx
+++ src/App.jsx
@@ -80,17 +80,19 @@ function Board({xIsNext , squares , onPlay}) {
 
 export default function Game(){
 
-  const [xIsNext , setXIsNext] = useState(true);
   const [history , setHistory] = useState([Array(9).fill(null)]); 
-  const currentSquares = history[history.length - 1];
+  const [currentMove , setCurrentMove] = useState(0);
+  const currentSquares = history[currentMove];
+  const xIsNext = currentMove % 2 === 0;
 
   function handlePlay(nextSquares){
-    setHistory([...history, nextSquares]);
-    setXIsNext(!xIsNext);
+    const nextHistroy = [...history.slice(0,currentMove + 1), nextSquares];
+    setHistory(nextHistroy);
+    setCurrentMove(nextHistroy.length - 1);
   }
 
   function jumpTo(nextMove){
-
+    setCurrentMove(nextMove);
   }
 
   const moves = history.map((squares,move) => {
